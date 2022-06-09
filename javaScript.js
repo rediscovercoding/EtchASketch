7734bc8f489d882a3b676e2262044f448495d8f3
kd <koustubhdube@outlook.com>
2022-06-09 16:09:33 +0200

    Add grid elements

--- javaScript.js
+++ javaScript.js
@@ -1,6 +1,23 @@
+const grid= document.querySelector('#grid');
+    
+    for (let n = 0; n < (16*16); n++){
+        div2 = document.createElement('div');
+        grid.appendChild(div2);
+        
+        div2.style.cssText = 'width: 10px; height: 10px';
+        
+    }
+    document.getElementById("grid").style.gridTemplateColumns = "repeat(16,10px)";
 
-function createGrid16(gridSize){
-    const grid= document.querySelector('#grid');
+document.getElementById("grid").onmouseover = function() {mouseOver()};
+function mouseOver() {
+    document.getElementById("grid").style.backgroundColor = "blue";
+    console.log('mouseover')
+  }
+
+
+function createGrid(gridSize){
+    
     
     for (let n = 0; n < (gridSize*gridSize); n++){
         div2 = document.createElement('div');
